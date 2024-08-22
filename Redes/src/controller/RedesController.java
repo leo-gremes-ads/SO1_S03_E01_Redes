90debeeab28b26109c3f74e363122b5fdd9087ae
Léo Gremes <160618417+leo-gremes-ads@users.noreply.github.com>
2024-08-21 23:36:15 -0300

    add ping function

---
+++ Redes/src/controller/RedesController.java
@@ -0,0 +1,63 @@
+package controller;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class RedesController
+{
+    public RedesController()
+    {
+        super();
+    }
+
+    private String os()
+    {
+        return System.getProperty("os.name");
+    }
+
+    public void ip()
+    {
+        return;
+    }
+
+    public void ping()
+    {
+        String proc;
+        if (os().contains("Windows"))
+            proc = "ping -4 -n 10 www.google.com.br";
+        else
+            proc = "ping -4 c 10 www.google.com.br";
+        String[] procArr = proc.split(" ");
+        try {
+            Process p = Runtime.getRuntime().exec(procArr);
+            InputStreamReader procOut = new InputStreamReader(p.getInputStream());
+            BufferedReader buffer = new BufferedReader(procOut);
+            String line = buffer.readLine();
+            if (os().contains("Windows")) {
+                while (line != null) {
+                    if (line.contains("ms")) {
+                        if (!line.contains("TTL"))
+                            System.out.println("\nPing médio: " + line.split(",")[2].split(" = ")[1]);
+                        else
+                            System.out.print(".");
+                    }
+                    line = buffer.readLine();
+                }
+            } else {
+                while (line != null) {
+                    if (line.contains("ms")) {
+                        if (line.contains("rtt"))
+                            System.out.println("\nPing médio: " + line.split("/")[4] + "ms");
+                        else
+                            System.out.print(".");
+                    }
+                    line = buffer.readLine();
+                }
+            }
+            procOut.close();
+            buffer.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
---
+++ Redes/src/view/Main.java
@@ -0,0 +1,29 @@
+package view;
+
+import controller.*;
+import javax.swing.JOptionPane;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        RedesController redes = new RedesController();
+        int opc = 0;
+        do {
+            opc = menu();
+            if (opc == 0)
+                continue;
+            if (opc == 1)
+                redes.ping();            
+        } while (opc != 2);
+    }
+
+    public static int menu()
+    {
+        String[] opcs = {"IP", "Ping", "Sair"};
+        return JOptionPane.showOptionDialog(
+            null, "Selecione a opção desejada", "Processos",
+            JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null,
+            opcs, opcs[2]);
+    }
+}
